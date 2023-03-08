a063915fbfbd58fa7a1805d78e8991d189e2cde3
Guilherme Landim <49691028+GuilhermeLandim@users.noreply.github.com>
2023-03-07 21:26:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
-var resultado;
+
+
 function verificarPalindromo(str) {
-    
+    var resultado;
     if (str == str.split('').reverse().join('')) {
         resultado = 'É palíndromo';
     } else {
         resultado = 'Não é palíndromo';
     }
+    document.getElementById("palavra").addEventListener("change", verificarPalindromo);
+    document.getElementById("resultado").value = resultado;
 }
-console.log(resultado)
+
+function addItem() {
+    if (document.getElementById("novoItem").value != '') {
+        let item = document.getElementById("novoItem").value;
+        let lista = document.getElementById("list");
+        let items = lista.getElementsByTagName("li");
+        let index = 0;
+        for (let i = 0; i < items.length; i++) {
+            if (item.toLowerCase() < items[i].innerHTML.toLowerCase()) {
+                index = i;
+                break;
+            }
+            index++;
+        }
+        let novaLinha = document.createElement("li");
+        let textNode = document.createTextNode(item);
+        novaLinha.appendChild(textNode);
+        lista.insertBefore(novaLinha, items[index]);
+        document.getElementById("novoItem").value = "";
+    }
+}
+document.getElementById("add-item").addEventListener("click", addItem);
 document.getElementById("palavra").addEventListener("change", verificarPalindromo);
-document.getElementById("resultado").value = resultado;
